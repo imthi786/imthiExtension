a0efd7dadfadb95374dacce7cfc695a71a4deec1
imthi786 <imthi.hhc@gmail.com>
2024-01-18 23:25:32 +0530

    [18-01-2024]-extension base

---
+++ poup.js
@@ -0,0 +1,7 @@
+// var $ = require( "jquery" );
+// $(document).ready(function () {
+    // Your jQuery code goes here
+    $('button').click(function () {
+        alert('Button clicked!');
+    });
+// });
